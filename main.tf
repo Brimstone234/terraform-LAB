e7f68d4a666fb49eb8f2c33a40724285e5783807
Denz Raymond Lacuesta <denz.lacuesta@d-connect.net>
2026-02-06 11:54:37 +0800

    add name tag to ec2 via main

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_subnet" {
 # 3. Create EC2 Instance
 resource "aws_instance" "demo" {
   ami           = "ami-062aea897630d952b"
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
 
   # This line connects your instance to the new infrastructure
   subnet_id = aws_subnet.public_subnet.id
